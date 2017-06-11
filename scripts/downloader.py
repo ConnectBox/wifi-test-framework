0a3bb4355686629438c9a733ad7e9d763240c16e
Edwin Steele <edwin-lists@wordspeak.org>
2017-06-11 16:37:28 +1000

    Use python throttling implementation, and no default network

--- scripts/downloader.py
+++ scripts/downloader.py
@@ -3,18 +3,76 @@
 import argparse
 from collections import Counter, OrderedDict
 import logging
+import math
 import pathlib
 import time
 import socket
-from subprocess import Popen, PIPE
 import sys
+import random
+import urllib.request
+import unittest
 
-UNLIMITED_SPEED = -1
+# 1GB/sec
+UNLIMITED_SPEED = 1000000000
+#
+TIMESLICE_SECS = 0.01
+BYTES_TO_READ = 1024
 
 LOGGER = logging.getLogger("downloader")
 
 
-# class OrderedCounter(Counter, OrderedDict):
+class TokenBucket(object):
+    """Derived from Alec Thomas' ActiveState Code Recipe
+
+    https://code.activestate.com/recipes/511490-implementation-of-the-token-bucket-algorithm/
+    """
+    def __init__(self,
+                 capacity,
+                 initial_tokens,
+                 refill_amount_per_interval,
+                 refill_interval_secs):
+        # Initial token count == capacity, for arguments sake
+        self.capacity = int(capacity)
+        self.initial_tokens = initial_tokens
+        self._tokens = max(self.capacity, float(initial_tokens))
+        self.refill_amount_per_interval = float(refill_amount_per_interval)
+        self.refill_interval_secs = float(refill_interval_secs)
+        self.last_refill_time = time.time()
+
+    def reset(self):
+        self._tokens = self.initial_tokens
+        self.last_refill_time = time.time()
+
+    def consume(self, tokens_requested):
+        # Can only consume whole numbers of tokens
+        assert isinstance(tokens_requested, int)
+        # Can only offer whole numbers of tokens
+        available_tokens = math.floor(self.available_tokens())
+        if tokens_requested > available_tokens:
+            self._tokens -= available_tokens
+            return available_tokens
+
+        self._tokens -= tokens_requested
+        return tokens_requested
+
+    def available_tokens(self):
+        if self._tokens < self.capacity:
+            now = time.time()
+            if (now - self.last_refill_time) >= self.refill_interval_secs:
+                # Only do refills until we hit a complete refill period
+                elapsed_interval_count = \
+                    (now - self.last_refill_time) / self.refill_interval_secs
+                new_tokens = \
+                    self.refill_amount_per_interval * elapsed_interval_count
+                # print("ROPI", self.refill_amount_per_interval, "eic",
+                #      elapsed_interval_count, "New tokens: ", new_tokens)
+                self._tokens = min(self.capacity, self._tokens + new_tokens)
+                # print("Tokens now:", self._tokens)
+                self.last_refill_time = now
+
+        return self._tokens
+
+
 class OrderedCounter(OrderedDict, Counter):
     "Counter that remembers the order elements are first encountered"
 
@@ -32,43 +90,42 @@ class Downloader(object):
         self.url = url
         self.rate_bps = rate_bps
         self.recvd_bytes_at_timestamp = OrderedCounter()
-        # Populated during run()
-        self.downloading_proc = None
-        # Populated during run()
-        self.test_identifier = None
-
-    def get_cmd(self):
-        cmd = ["wget",
-               "--quiet",
-               "--output-document=-",
-               self.url]
-
-        if self.rate_bps != UNLIMITED_SPEED:
-            # Insert as the first argument (right after the command)
-            cmd.insert(1, "--limit-rate={0:d}".format(self.rate_bps))
-
-        return cmd
+        # bucket is refilled every TIMESLICE_SECS with the
+        #  data rate at rate/sec * 1/TIMESLICE_SECS
+        self.download_quota = TokenBucket(
+            self.rate_bps, 0, self.rate_bps * TIMESLICE_SECS, TIMESLICE_SECS)
+        self.test_identifier = "{0}_{1}".format(socket.getfqdn(),
+                                                random.randint(0, pow(2, 24)))
+        # Populated in run()
+        self.remote_site = None
 
     def service_once(self):
         now_secs = int(time.time())
         if now_secs > self.last_timestamp:
-            self.downloading_proc.poll()
             LOGGER.info("%s - %s received bytes: %s",
                         self.last_timestamp,
                         self.test_identifier,
                         self.recvd_bytes_at_timestamp[self.last_timestamp])
-        newly_read = self.downloading_proc.stdout.read(1024)
-        self.recvd_bytes_at_timestamp[now_secs] += len(newly_read)
-        self.last_timestamp = now_secs
+        max_bytes_to_read = self.download_quota.consume(BYTES_TO_READ)
+        if max_bytes_to_read > 0:
+            newly_read = self.remote_site.read(max_bytes_to_read)
+            self.recvd_bytes_at_timestamp[now_secs] += len(newly_read)
+            self.last_timestamp = now_secs
+            return len(newly_read)
+
+        return 0
 
     def run(self):
-        self.downloading_proc = Popen(self.get_cmd(), stdout=PIPE, bufsize=0)
-        LOGGER.debug("Process running with PID: %s", self.downloading_proc.pid)
-        self.test_identifier = "{0}_{1}".format(socket.getfqdn(),
-                                                self.downloading_proc.pid)
+        self.remote_site = urllib.request.urlopen(self.url)
+        # Reset the counter when the URL has actually been opened otherwise
+        #  we accrue tokens while the connection is being established
+        self.download_quota.reset()
 
-        while self.downloading_proc.returncode is None:
-            self.service_once()
+        # XXX what happens if this is over a slow link?
+        while self.remote_site.length > 0:
+            bytes_read = self.service_once()
+            if bytes_read == 0:
+                time.sleep(TIMESLICE_SECS)
 
     def report(self, stats_fd):
         LOGGER.debug("Writing stats to %s", stats_fd.name)
@@ -146,6 +203,81 @@ def main():
         stats_fd = open(str(output_path), "w")
 
     dler.report(stats_fd)
+
+
+class TokenBucketTestCase(unittest.TestCase):
+    def testDryBucket(self):
+        tb = TokenBucket(0, 0, 0, 0)
+        tokens = tb.consume(0)
+        self.assertEqual(tokens, 0)
+        # Wait, to demonstrate it doesn't refill
+        time.sleep(1)
+        tokens = tb.consume(0)
+        self.assertEqual(tokens, 0)
+
+    def testZeroCapacityBucket(self):
+        tb = TokenBucket(0, 0, 1, 1)
+        tokens = tb.consume(1)
+        self.assertEqual(tokens, 0)
+        # Wait, to demonstrate it doesn't refill
+        time.sleep(1)
+        tokens = tb.consume(0)
+        self.assertEqual(tokens, 0)
+
+    def testIntegerRefill(self):
+        one_per_sec = TokenBucket(1, 1, 1, 1)
+        tokens = one_per_sec.consume(1)
+        self.assertEqual(tokens, 1)
+        # refill (1 token per second)
+        time.sleep(1)
+        tokens = one_per_sec.consume(1)
+        self.assertEqual(tokens, 1)
+
+    def testFulfilPartialRequest(self):
+        one_per_sec = TokenBucket(1, 1, 1, 1)
+        tokens = one_per_sec.consume(2)
+        self.assertEqual(tokens, 1)
+        # refill (1 token per second)
+        time.sleep(1)
+        tokens = one_per_sec.consume(2)
+        self.assertEqual(tokens, 1)
+
+    def testFloatRefillRate(self):
+        half_per_sec = TokenBucket(1, 1, 0.5, 1)
+        # Consume initial capacity
+        tokens = half_per_sec.consume(1)
+        self.assertEqual(tokens, 1)
+        # Confirm empty
+        tokens = half_per_sec.consume(1)
+        self.assertEqual(tokens, 0)
+        # wait for a second for a refill
+        time.sleep(1)
+        tokens = half_per_sec.consume(1)
+        # Can only distribute whole numbers of tokens
+        self.assertEqual(tokens, 0)
+        # Wait for a second so we have an integer number of tokens available
+        time.sleep(1)
+        tokens = half_per_sec.consume(1)
+        self.assertEqual(tokens, 1)
+
+    def testGT1secRefillRate(self):
+        two_per_two_secs = TokenBucket(2, 2, 2, 2)
+        # Consume initial capacity
+        tokens = two_per_two_secs.consume(2)
+        self.assertEqual(tokens, 2)
+        # Confirm empty
+        tokens = two_per_two_secs.consume(1)
+        self.assertEqual(tokens, 0)
+        # wait for a second
+        time.sleep(1)
+        # Confirm still empty (2 sec refill period)
+        tokens = two_per_two_secs.consume(1)
+        self.assertEqual(tokens, 0)
+        # wait for a second
+        time.sleep(1)
+        # Confirm refill
+        tokens = two_per_two_secs.consume(2)
+        self.assertEqual(tokens, 2)
 
 
 if __name__ == "__main__":
--- scripts/run.sh
+++ scripts/run.sh
@@ -5,25 +5,11 @@ WAIT_TIME_FOR_ASSOCIATION_SECS=20
 now_secs=$(date +"%s")
 test_start_time=$(( $now_secs+$WAIT_TIME_FOR_ASSOCIATION_SECS ))
 
-wifi_network_under_test=$(awk -F " = " '$1 = /wifi_network_under_test/ { print $2; }' /etc/wifi_test_framework.conf)
-wifi_network_default_name=$(awk -F " = " '$1 = /wifi_network_default_name/ { print $2; }' /etc/wifi_test_framework.conf)
-wifi_network_default_password=$(awk -F " = " '$1 = /wifi_network_default_password/ { print $2; }' /etc/wifi_test_framework.conf)
-
 # Only care about the first wlan device
 wlan_device=$(ip -o link | awk -F: '$2 ~ /wl.*/ {print $2;}' | head -1);
 
-echo -n "Before: "
-iwconfig ${wlan_device} | head -1
-current_wifi=$(iwconfig ${wlan_device} | head -1 | cut -d'"' -f2)
-
-if [ "${current_wifi}" != "${wifi_network_under_test}" ]; then
-    echo "Disconnecting from ${current_wifi} and connecting to ${wifi_network_under_test}"
-    # Disconnect previous connection
-    nmcli device disconnect ${wlan_device}
-    # Connect to the network under test
-    nmcli device wifi connect "${wifi_network_under_test}" \
-        ifname ${wlan_device}
-fi
+echo "Connecting to network under test"
+ifdown ${wlan_device}; sleep 1; ifup ${wlan_device}
 
 echo -n "During: "
 iwconfig ${wlan_device} | head -1
@@ -44,16 +30,9 @@ done
 
 wait
 
-if [ "${current_wifi}" != "${wifi_network_default_name}" ]; then
-    echo "Disconnecting from ${current_wifi} and connecting to ${wifi_network_default_name}"
-    # Disconnect from the network under test
-    nmcli device disconnect ${wlan_device}
-    # Reconnect to the default network
-    nmcli device wifi \
-        connect "${wifi_network_default_name}" \
-        password "${wifi_network_default_password}" \
-        ifname ${wlan_device}
-fi
+echo "Disconnecting from network under test"
+ifdown ${wlan_device}
 
 echo -n "After: "
 iwconfig ${wlan_device} | head -1
+
