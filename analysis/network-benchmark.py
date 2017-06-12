7038717e8af9a3e5f0ba02e4a6b06a1830c4c9a1
Edwin Steele <edwin-lists@wordspeak.org>
2017-06-12 15:12:45 +1000

    Programatically create graph title

--- analysis/network-benchmark.py
+++ analysis/network-benchmark.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import configparser
 import glob
 import io
 
@@ -7,8 +8,10 @@ import pandas as pd
 import numpy as np
 
 
-TEST_RESULT_FILES_PATTERN = \
-    "/Users/esteele/tmp/wifi-test/root/wifi-test-results/test-run-{0}/*.csv"
+TEST_RESULT_DIR = \
+    "/Users/esteele/tmp/wifi-test/root/wifi-test-results/test-run-{0}"
+TEST_RESULT_METADATA_FILE = TEST_RESULT_DIR + "/metadata.ini"
+TEST_RESULT_FILES_PATTERN = TEST_RESULT_DIR + "/*.csv"
 
 
 def get_test_run_results(test_run_id):
@@ -43,13 +46,34 @@ def get_tick_labels(min_value, max_value, labels_count):
             [max_value]
 
 
+def get_graph_title_for_run(test_run_id):
+    config = configparser.ConfigParser()
+    config.read(TEST_RESULT_METADATA_FILE.format(test_run_id))
+    # Don't count the global section
+    client_count = len(config.sections()) - 1
+    stream_count = sum([int(config[s]["parallel_run_count"])
+                        for s in config.sections()
+                        if s != "global"])
+    bandwidth_desc = config["global"]["test_bandwidth"]
+    title = "Run {0} against {1}{2}.\n{3} clients ({4} streams @ {5} each)" \
+            .format(
+                test_run_id,
+                config["global"]["test_server_hostname"],
+                config["global"]["extra_run_description"],
+                client_count,
+                stream_count,
+                bandwidth_desc
+            )
+    return title
+
+
 def show_run_df_as_line_graph(df, title):
     pivot_df = df.pivot(index="time_offset",
                         columns="client_id",
                         values="bytes_per_sec")
     ax = pivot_df.plot(figsize=(10, 10))
     ax.set_xlabel("Elapsed time (sec)")
-    x.set_xbound(lower=0)
+    ax.set_xbound(lower=0)
     ax.set_ylabel("Throughput (bytes/sec)")
     ax.axhline(y=250000,
                color='0.75',
@@ -75,7 +99,7 @@ def show_run_df_as_boxplot(df, title):
     ax2.annotate("       480p bitrate", (max(df["time_offset"]), 250000))
     ax2.set_title(title)
 
-#df1 = get_dataframe_from_test_run(1234)
-#df2 = get_dataframe_from_test_run(5678)
-#df1_and_df2 = pd.concat([df1, df2])
-#df1_and_df2["test_case"] = "MAF1"
+# df1 = get_dataframe_from_test_run(1234)
+# df2 = get_dataframe_from_test_run(5678)
+# df1_and_df2 = pd.concat([df1, df2])
+# df1_and_df2["test_case"] = "MAF1"
